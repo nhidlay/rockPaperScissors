235efc9211ae9d7e302e9f14f86e3e721015a5fb
pilmar <nhidlay@gmail.com>
2022-05-21 09:50:42 -0600

    Added playRound function that plays one round of rock, paper, scissors.

--- game.js
+++ game.js
@@ -21,10 +21,21 @@ function computerPlay()
 
 function playRound (playerSelection, computerSelection)
 {
-    let playerSelection = "rock";
-    let computerSelection = computerPlay();
+    playerSelection = "rock";
+    computerSelection = computerPlay();
 
-
+    if (playerSelection == "rock" && computerSelection == "rock")
+    {
+        return "It's a tie!";
+    }
+    else if (playerSelection == "rock" && computerSelection == "paper")
+    {
+        return "You lose! Paper beats rock!";
+    }
+    else if (playerSelection == "rock" && computerSelection == "scissors")
+    {
+        return "You Win! Rock beats scissors!";
+    }
 }
 
-//console.log(computerPlay());
+console.log(playRound());
